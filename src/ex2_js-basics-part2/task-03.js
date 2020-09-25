57a2681e31e5e86efaf8e83c8994d23039e20e69
serj <olchievsereja@mail.ru>
2020-09-25 23:51:04 +0300

    Task 3 is done

---
+++ src/ex2_js-basics-part2/task-03.js
@@ -0,0 +1,25 @@
+let f = function(arr) {
+    //кол-во нечетных чисел
+    let oddNumCount = 0;
+    //кол-во четныхs чисел
+    let evenNumCount = 0;
+    //кол-во нулей
+    let zeroNumCount = 0;
+    arr.forEach( (element) => {
+        if (typeof element == 'number' && !Number.isNaN(element)) {
+            if (element == 0) {
+                zeroNumCount += 1;
+            } else if (element % 2 == 0) {
+                evenNumCount += 1;
+            } else {
+                oddNumCount += 1;
+            }
+        }        
+    });
+
+    return [evenNumCount, oddNumCount, zeroNumCount];
+};
+
+
+
+module.exports = f;
