47a553420074e2da7346384bd8f2fb00693d8b8a
serj <olchievsereja@mail.ru>
2020-09-27 18:59:49 +0300

    Update Task 2

--- src/ex3_js-objects-part1/task-02.js
+++ src/ex3_js-objects-part1/task-02.js
@@ -1,8 +1,10 @@
 let f = function(obj) {
     for (const key in obj) {
-      console.log(key + ':' + obj[key]);     
+      if (obj.hasOwnProperty(key)) {
+        console.log(key + ':' + obj[key]);
+      }     
     }
-    
+
     return;
 };
 
