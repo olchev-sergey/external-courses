2ae546da7deed08e111f13693c42f96cd2f49861
serj <olchievsereja@mail.ru>
2020-10-06 23:33:07 +0300

    Task 3 is done

---
+++ src/ex6_js-array-methods/task-03.js
@@ -0,0 +1,11 @@
+const every = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (!callback(array[i], i, array)) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+module.exports = every;
