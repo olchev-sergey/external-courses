7a5ddad5d13fd8f5c1b0044f192b52f0771fda4e
serj <olchievsereja@mail.ru>
2020-10-06 20:58:25 +0300

    Update task 3

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,10 @@
 const deleteSpaces = (str) => {
-    return str.trim();
+    const start = str[0] === ' ' ? 1 : 0;
+    
+    const end = str[str.length - 1] === ' ' ? -1 : undefined;
+
+    return str.slice(start, end);
+
 };
 
 module.exports = deleteSpaces;
