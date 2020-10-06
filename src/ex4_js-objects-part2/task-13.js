ce79447734e052091b872de773bcd04dc044825f
serj <olchievsereja@mail.ru>
2020-10-06 21:11:13 +0300

    Update task 13 and 14

--- src/ex4_js-objects-part2/task-13.js
+++ src/ex4_js-objects-part2/task-13.js
@@ -1,5 +1,5 @@
 const random = () => {
-    return Math.floor(Math.random() * 100);
+    return Math.floor(Math.random() * 100 + 1);
 };
 
 module.exports = random;
--- src/ex4_js-objects-part2/task-14.js
+++ src/ex4_js-objects-part2/task-14.js
@@ -1,5 +1,5 @@
 const randomInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 module.exports = randomInRange;
