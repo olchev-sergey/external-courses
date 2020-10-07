c0e9ccf6440619ea18fbe993859a159212b18fed
serj <olchievsereja@mail.ru>
2020-10-08 00:03:58 +0300

    Update task

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-
+/*
 class Calculator {
     constructor (){
         this.result = 0;
@@ -55,9 +55,148 @@ class Calculator {
         
         return f;
     }
+
+    divide(num) {
+        num = num || 0;
+
+        this.result /= num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result /= num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+    }
+
+    multiply(num) {
+        num = num || 0;
+
+        this.result *= num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result *= num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+    }
+    
     
 }
+*/
 
+function Calculator() {
+    this.result = 0;
 
-const calculator = new Calculator();
+    this.getResult = function () {
+        return this.result;
+    };
+
+    this.reset = function() {
+        this.result = 0;
+
+        return this.result;
+    };
+
+    this.add = function (num) {
+        num = num || 0;       
+
+        this.result += num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result += num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+
+    };
+
+    this.subtract = function (num) {
+        num = num || 0;
+
+        this.result -= num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result -= num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+    };
+
+    this.divide = function (num) {
+        num = num || 0;
+
+        this.result /= num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result /= num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+    };
+
+    this.multiply = function (num) {
+        num = num || 0;
+
+        this.result *= num;
+
+        const f = (num2) => {
+            num2 = num2 || 0;
+
+            this.result *= num2;
+
+            return f;
+        };
+
+        f.toString = () => {
+            return this.result;
+        };
+        
+        return f;
+    }; 
+}
+
+// const calculator = new Calculator();
+// calculator.add(3)(5);
+// console.log(calculator.getResult());
+module.exports = new Calculator;
 
