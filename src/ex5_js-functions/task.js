61aca213b6800f03808982f64df13f78ad1391f1
serj <olchievsereja@mail.ru>
2020-10-09 01:14:07 +0300

    Update task

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
-/*
+
 class Calculator {
-    constructor (){
+    constructor () {
         this.result = 0;
     }
 
@@ -98,105 +98,5 @@ class Calculator {
     
     
 }
-*/
 
-function Calculator() {
-    this.result = 0;
-
-    this.getResult = function () {
-        return this.result;
-    };
-
-    this.reset = function() {
-        this.result = 0;
-
-        return this.result;
-    };
-
-    this.add = function (num) {
-        num = num || 0;       
-
-        this.result += num;
-
-        const f = (num2) => {
-            num2 = num2 || 0;
-
-            this.result += num2;
-
-            return f;
-        };
-
-        f.toString = () => {
-            return this.result;
-        };
-        
-        return f;
-
-    };
-
-    this.subtract = function (num) {
-        num = num || 0;
-
-        this.result -= num;
-
-        const f = (num2) => {
-            num2 = num2 || 0;
-
-            this.result -= num2;
-
-            return f;
-        };
-
-        f.toString = () => {
-            return this.result;
-        };
-        
-        return f;
-    };
-
-    this.divide = function (num) {
-        num = num || 0;
-
-        this.result /= num;
-
-        const f = (num2) => {
-            num2 = num2 || 0;
-
-            this.result /= num2;
-
-            return f;
-        };
-
-        f.toString = () => {
-            return this.result;
-        };
-        
-        return f;
-    };
-
-    this.multiply = function (num) {
-        num = num || 0;
-
-        this.result *= num;
-
-        const f = (num2) => {
-            num2 = num2 || 0;
-
-            this.result *= num2;
-
-            return f;
-        };
-
-        f.toString = () => {
-            return this.result;
-        };
-        
-        return f;
-    }; 
-}
-
-// const calculator = new Calculator();
-// calculator.add(3)(5);
-// console.log(calculator.getResult());
-module.exports = new Calculator;
-
+module.exports = Calculator;
