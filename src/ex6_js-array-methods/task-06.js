88aa180eeb09a279184fa0ebc026e2150c8c31db
serj <olchievsereja@mail.ru>
2020-10-13 10:41:56 +0300

    Update task 6

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,5 +1,5 @@
 const reduce = (array, callback, initialValue) => {
-    const start = initialValue === 'undefined' ? 1 : 0;
+    const start = initialValue === undefined ? 1 : 0;
     let previousValue = start === 1 ? array[0] : initialValue;
 
     for (let i = start; i < array.length; i++) {
