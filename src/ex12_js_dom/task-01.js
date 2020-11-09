22208c554eb11ebe7cb2f660d2ac58e8251524aa
serj <olchievsereja@mail.ru>
2020-11-10 00:21:03 +0300

    Add slider-animation and accessibility

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,3 +1,5 @@
+/*eslint no-param-reassign: ["error", { "props": false }]*/
+
 const imgUrlArr = [
     './asset/EPAM_office.jpg',
     './asset/logo.png',
@@ -8,19 +10,20 @@ const copyImgUrlArr = [...imgUrlArr];
 
 window.onload = () => {
     const sliderImage = document.querySelector('.slider-image');
-    sliderImage.style.backgroundImage = `url(${copyImgUrlArr[0]}`;
-
     const arrowLeft = document.querySelector('.arrow-left');
     const arrowRight = document.querySelector('.arrow-right');
 
     arrowLeft.addEventListener('click', () => {
         copyImgUrlArr.unshift(copyImgUrlArr.pop());
         sliderImage.style.backgroundImage = `url(${copyImgUrlArr[0]})`;
+        animSlider(sliderImage, -100, 500);
+
     }, false);
 
     arrowRight.addEventListener('click', () => {
         copyImgUrlArr.push(copyImgUrlArr.shift());
         sliderImage.style.backgroundImage = `url(${copyImgUrlArr[0]})`;
+        animSlider(sliderImage, 100, 500);
     }, false);
 
     document.addEventListener('keydown', (e) => {
@@ -28,4 +31,27 @@ window.onload = () => {
             document.activeElement.click();
         }
     }, false);
+
+    document.querySelector('.slider-image-wrapper').addEventListener('focus', () => {
+        arrowLeft.focus();
+    });
 };
+
+function animSlider(imgBlock, startPosLeft, animDuration) {
+    imgBlock.style.left = startPosLeft + '%';
+    const timeInterval = animDuration / 100;
+    const posStep = startPosLeft / 100 * -1;
+    let posLeft = startPosLeft;
+
+    const timer = setInterval(() => {
+        posLeft += posStep;
+
+        if (posLeft === 0 + posStep ) {
+            clearInterval(timer);
+
+            return;
+        }
+
+        imgBlock.style.left = posLeft + '%';
+    }, timeInterval);
+}
