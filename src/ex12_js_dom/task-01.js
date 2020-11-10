c0863cd8148674dcfc4f4c75e77545d29ee86852
serj <olchievsereja@mail.ru>
2020-11-10 21:33:04 +0300

    Update slider animation

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -9,21 +9,23 @@ const imgUrlArr = [
 const copyImgUrlArr = [...imgUrlArr];
 
 window.onload = () => {
-    const sliderImage = document.querySelector('.slider-image');
+    const sliderImage = document.querySelector('.main-image');
+    const bogusImage = document.querySelector('.bogus-image');
     const arrowLeft = document.querySelector('.arrow-left');
     const arrowRight = document.querySelector('.arrow-right');
 
     arrowLeft.addEventListener('click', () => {
         copyImgUrlArr.unshift(copyImgUrlArr.pop());
         sliderImage.style.backgroundImage = `url(${copyImgUrlArr[0]})`;
-        animSlider(sliderImage, -100, 500);
-
+        bogusImage.style.backgroundImage = `url(${copyImgUrlArr[1]})`;
+        moveAnim(sliderImage, bogusImage, -100, 100, 'right', 500);
     }, false);
 
     arrowRight.addEventListener('click', () => {
         copyImgUrlArr.push(copyImgUrlArr.shift());
         sliderImage.style.backgroundImage = `url(${copyImgUrlArr[0]})`;
-        animSlider(sliderImage, 100, 500);
+        bogusImage.style.backgroundImage = `url(${copyImgUrlArr[copyImgUrlArr.length -1]})`;
+        moveAnim(sliderImage, bogusImage, 100, 100, 'left', 500);
     }, false);
 
     document.addEventListener('keydown', (e) => {
@@ -37,21 +39,33 @@ window.onload = () => {
     });
 };
 
-function animSlider(imgBlock, startPosLeft, animDuration) {
+
+function moveAnim(mainImage, bogusImage, startPosLeft,moveLength, direction, animDuration) {
+    animSlider(bogusImage, 0,moveLength, direction, animDuration);
+    animSlider(mainImage, startPosLeft,moveLength, direction, animDuration);
+}
+
+function animSlider(imgBlock, startPosLeft,moveLength, direction, animDuration) {
     imgBlock.style.left = startPosLeft + '%';
     const timeInterval = animDuration / 100;
-    const posStep = startPosLeft / 100 * -1;
+    let posStep;
+
+    if (direction === 'left') {
+        posStep = -1;
+    } else if (direction === 'right') {
+        posStep = 1;
+    }
+
     let posLeft = startPosLeft;
 
     const timer = setInterval(() => {
         posLeft += posStep;
+        imgBlock.style.left = posLeft + '%';
 
-        if (posLeft === 0 + posStep ) {
+        if (posLeft === (startPosLeft + (posStep * moveLength))) {
             clearInterval(timer);
 
             return;
         }
-
-        imgBlock.style.left = posLeft + '%';
     }, timeInterval);
 }
