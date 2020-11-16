0e4f09b48696b1dcb1c84417984136dda3034d50
serj <olchievsereja@mail.ru>
2020-11-16 20:32:06 +0300

    Add mock.js

--- src/kanban/main-block.js
+++ src/kanban/main-block.js
@@ -1,91 +1,6 @@
-const dataMock = [
-    {
-        title: 'Backlog',
-        issues: [
-            {
-                id: 'task1',
-                name: 'Login page – performance issues',
-            },
-            {
-                id: 'task2',
-                name: 'Sprint bugfix',
-            },
-            {
-                id: 'task3',
-                name: 'Sprdsdsddsint bugfix',
-            },
-        ],
-    },
-    {
-        title: 'Ready',
-        issues: [
-            {
-                id: 'task1',
-                name: 'Shop page – performance issues',
-            },
-            {
-                id: 'task2',
-                name: 'Checkout bugfix',
-            },
-            {
-                id: 'task3',
-                name: 'Shop bug1',
-            },
-            {
-                id: 'task4',
-                name: 'Shop bug2',
-            },
-            {
-                id: 'task5',
-                name: 'Shop bug3',
-            },
-            {
-                id: 'task6',
-                name: 'Shop bug4',
-            },
-            {
-                id: 'task7',
-                name: 'Shop bug5',
-            },
-            {
-                id: 'task8',
-                name: 'Shop bug6',
-            },
-            {
-                id: 'task9',
-                name: 'Shop page – performance issues',
-            },
-        ],
-    },
-    {
-        title: 'In Progress',
-        issues: [
-            {
-                id: 'task1',
-                name: 'User page – performance issues',
-            },
-            {
-                id: 'task2',
-                name: 'Auth bugfix',
-            },
-        ],
-    },
-    {
-        title: 'Finished',
-        issues: [
-            {
-                id: 'task1',
-                name: 'Main page – performance issues',
-            },
-            {
-                id: 'task2',
-                name: 'Main page bugfix',
-            },
-        ],
-    },
-];
+const dataMock = JSON.parse(localStorage.getItem('dataMock'));
 
-const getDataValueArr = (dataMock) => {
+const getDataMockValueArr = (dataMock) => {
     const result = [];
 
     dataMock.forEach((taskBlock) => {
@@ -104,7 +19,7 @@ const createLi = constructorCreateLi('task-block__list-item');
 const createInput = constructorCreateInput('task-block__input');
 const createSelect = constructorCreateSelect('task-block__select');
 
-const ulValueArr = getDataValueArr(dataMock);
+const ulValueArr = getDataMockValueArr(dataMock);
 
 const taskBlockArr = document.querySelectorAll('.task-block');
 
---
+++ src/kanban/mock.js
@@ -0,0 +1,92 @@
+const dataMockStorage = [
+    {
+        title: 'Backlog',
+        issues: [
+            {
+                id: 'task1',
+                name: 'Login page – performance issues',
+            },
+            {
+                id: 'task2',
+                name: 'Sprint bugfix',
+            },
+            {
+                id: 'task3',
+                name: 'Sprdsdsddsint bugfix',
+            },
+        ],
+    },
+    {
+        title: 'Ready',
+        issues: [
+            {
+                id: 'task1',
+                name: 'Shop page – performance issues',
+            },
+            {
+                id: 'task2',
+                name: 'Checkout bugfix',
+            },
+            {
+                id: 'task3',
+                name: 'Shop bug1',
+            },
+            {
+                id: 'task4',
+                name: 'Shop bug2',
+            },
+            {
+                id: 'task5',
+                name: 'Shop bug3',
+            },
+            {
+                id: 'task6',
+                name: 'Shop bug4',
+            },
+            {
+                id: 'task7',
+                name: 'Shop bug5',
+            },
+            {
+                id: 'task8',
+                name: 'Shop bug6',
+            },
+            {
+                id: 'task9',
+                name: 'Shop page – performance issues',
+            },
+        ],
+    },
+    {
+        title: 'In Progress',
+        issues: [
+            {
+                id: 'task1',
+                name: 'User page – performance issues',
+            },
+            {
+                id: 'task2',
+                name: 'Auth bugfix',
+            },
+        ],
+    },
+    {
+        title: 'Finished',
+        issues: [
+            {
+                id: 'task1',
+                name: 'Main page – performance issues',
+            },
+            {
+                id: 'task2',
+                name: 'Main page bugfix',
+            },
+        ],
+    },
+];
+
+if (!localStorage.getItem('dataMock')) {
+    localStorage.setItem('dataMock', JSON.stringify(dataMockStorage));
+}
+
+// localStorage.clear();
